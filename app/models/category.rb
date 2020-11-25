b9187f1fc57642168c2754172c4cc113beef6ad3
Fish234 <hourglassbranch25@gmail.com>
2020-11-24 23:06:50 -0300

    belongs_to and has_many

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :products
+  belongs_to :parent_category, class_name: 'Category', optional: true
+  has_many :children, class_name: 'Category', foreign_key: 'parent_category_id'
 end
