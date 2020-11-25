2ede3cf0cbdb1d9407508ba87740773bfcfe2b1c
Fish234 <hourglassbranch25@gmail.com>
2020-11-25 01:30:49 -0300

    children stock

--- app/models/base_product.rb
+++ app/models/base_product.rb
@@ -1,2 +1,9 @@
 class BaseProduct < ApplicationRecord
+    has_many :products
+    def children
+        children = self.products
+    end
+    def children_stock
+        self.products.sum(:stock)
+    end
 end
